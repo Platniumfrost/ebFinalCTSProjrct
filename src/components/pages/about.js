373bf360d66b2698dc61e56e4b21bfbeaeaf8506
Ericsb52 <broadbent52@aol.com>
2021-07-14 08:08:31 -0600

    finalproject

---
+++ src/components/pages/about.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import profilePicture from "../../../static/assets/images/bio/headshot.jpg";
+
+export default function(){
+    return(
+        <div className="content-page-wrapper">
+            <div 
+            className="left-column"
+            style={{
+                background: "url(" + profilePicture + ") no-repeat",
+                backgroundSize: "cover",
+                backgroundPosition: "center"
+            }}
+            />
+            <div className="right-column">
+                kalkfdva dlaksjdf awlksjfakwsjdf awedkfjaodjfpoadfoajwdf alsdkjfawkjefpowahefoadojfpaowikdjfa
+                aldskjfpaowkejfpoajwrpeogfnapijfrwngpiajenrfg ;aokwdjfg aoierwjg a;ekfrjg aoierwjg a
+                kfhwapoir gfqoeikjfdg aoierfjhg poeaijdr gfpqafehg peojr gtoaierjfg powaierjg piuewsahr f
+                ahgpoerhgpsoejfg oqaewijf paoifdgh peoifg oeraifg pqoiaeru tw
+                a gsaoifegh pwsoifejg paoifejg posafhg oisnfbpoi wsjahfrgpo sadfp a
+                asfog jpaogfuhbpwa oifehg pskjvfnb pwasufrn qa pokedwjfg piwsfj 
+            </div>
+        </div>
+    )
+}
